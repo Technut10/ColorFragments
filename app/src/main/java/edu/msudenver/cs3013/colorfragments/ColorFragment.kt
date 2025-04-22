90604a6911ba91191d0ecfedad97a984276c8fe1
Tony <59328777+Technut10@users.noreply.github.com>
2025-04-21 23:34:26 -0600

    Initial commit

---
+++ app/src/main/java/edu/msudenver/cs3013/colorfragments/ColorFragment.kt
@@ -0,0 +1,80 @@
+package edu.msudenver.cs3013.colorfragments
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ColorFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ColorFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+     var myIndex : Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color, container, false)
+    }
+
+    override fun onViewCreated(view:View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("BlueFragment", "===ON VIEW CREATED!!!====")
+        view.setBackgroundColor(Color.rgb(150,150,255))
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ColorFragment.
+         */
+            var fragmentCount : Int
+                get(){
+                    return fragmentCount++
+                }
+            init {
+                fragmentCount = 0
+            }
+
+
+
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ColorFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+                 myIndex = fragmentCount
+
+            }
+    }
+}
